1ea0db4c490cca8d74eea2600717c2046f8ac985
Steve Flanagan <flanagan.s@husky.neu.edu>
2023-04-05 23:18:47 -0400

    spacing + commit to single quotes

--- get_vanity_address.py
+++ get_vanity_address.py
@@ -6,10 +6,10 @@ def generate_eth_wallet():
   vk = sk.get_verifying_key()
   k = keccak.new(digest_bits=256)
   k.update(vk.to_string())
-  return {"public_address":'0x' + k.hexdigest()[24:], "private_key": sk.to_string().hex() }
+  return { 'public_address' : '0x' + k.hexdigest()[24:], 'private_key' : sk.to_string().hex() }
 
 def get_vanity_address(vanity_str):
-  returned_wallet = { "public_address": '', "private_key": ''}
+  returned_wallet = { 'public_address' : '', 'private_key' : '' }
   while returned_wallet['public_address'][:len(vanity_str)] != vanity_str:
     returned_wallet = generate_eth_wallet()
   return returned_wallet
